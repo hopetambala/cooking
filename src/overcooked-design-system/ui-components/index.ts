b55e7156a9f6d66a544dd5f43f2bfec6f64694cf
Hope Tambala <hopetambala@outlook.com>
2024-08-06 23:50:05 -0400

    footer

--- src/overcooked-design-system/ui-components/index.ts
+++ src/overcooked-design-system/ui-components/index.ts
@@ -1,5 +1,5 @@
 export { default as OCHeader } from "./layout/header/OCHeader";
-export { default as OCFooter } from "./layout/footer/OCFooter";
+export { default as OCFooter } from "./layout/footer/OcFooter";
 export { default as OcSection } from "./layout/section/OcSection";
 export { default as OcGrid } from "./layout/grid/OcGrid";
 
--- src/overcooked-design-system/ui-components/layout/footer/OCFooter.tsx
+++ src/overcooked-design-system/ui-components/layout/footer/OCFooter.tsx
@@ -1,10 +1,10 @@
-export default function OCFooter() {
+import styles from './OcFooter.module.css';
+
+export default function OcFooter() {
   return (
-    <footer className="border-t border-zinc-800 mt-44">
-      <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400">
-        <small className=" duration-200 font-mono">
-          All rights reserved &copy; {new Date().getFullYear()}
-        </small>
+    <footer className={styles["oc-footer__container"]}>
+      <div>
+        <small>{new Date().getFullYear()} Puente Cooking. All rights reserved &copy; </small>
       </div>
     </footer>
   );
